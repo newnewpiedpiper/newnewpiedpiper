0fdfa12524b78cf43ec2f2c3500069bb4f57479c
EC2 Default User <ec2-user@ip-172-31-43-170.us-east-2.compute.internal>
2019-01-08 01:07:43 +0000

    "sessions"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
 
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   root 'posts#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
