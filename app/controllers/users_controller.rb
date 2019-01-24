ad0b4a344f8a07f478cb5b48d169eaaf5d8fa0cc
EC2 Default User <ec2-user@ip-172-31-43-170.us-east-2.compute.internal>
2019-01-24 01:03:13 +0000

    updates and all users work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  
+   before_action :logged_in_user, only: [:index, :edit, :update]
+  
+  
+  def index
+    @users = User.all
+  end
+  
+  
   def name
   end
 
@@ -35,6 +44,8 @@ class UsersController < ApplicationController
   @user = User.find_by_username(params[:username])
      if @user.update_attributes(user_params)
       # Handle a successful update.
+       flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
      end
@@ -48,6 +59,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :username, :email, :password,
                                    :password_confirmation)
     end 
+    
+     # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
-                          
+
+    
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   #                   uniqueness: { case_sensitive: false }
                     
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :upvotes, dependent: :destroy
