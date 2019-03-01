1ceb1afbd61b20f45f8d4fa7d77b69b1b09c5017
easter97 <easter97@tamu.edu>
2019-03-01 20:39:46 +0000

    Latest posts first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,10 @@ class PostsController < ApplicationController
             @posts = Post.all
             
             # @posts = Post.order('created_at DESC')
-            @posts=Post.order('cast(created_at as date) asc, cached_votes_score desc')
+            @posts=Post.order('cast(created_at as date) desc, cached_votes_score desc')
         else
             subs=current_user.subscriptions.split(',')
-            @posts = Post.where(channel_id: subs).order('cast(created_at as date) asc, cached_votes_score desc')
+            @posts = Post.where(channel_id: subs).order('cast(created_at as date) desc, cached_votes_score desc')
         end
 
     end
